ab45f1dfeafbcb4489cfb6301989c55f69fd6ba8
tallman <vedunaik@gmail.com>
2024-03-28 12:21:42 +0530

    solved the issue

--- sum(1).js
+++ sum(1).js
@@ -1,2 +1,22 @@
-let sum = a => b => b ? sum(a + b) : a;
-console.log(sum(1)(2)(3)(4)());
+// let sum = a => b => b ? sum(a + b) : a;
+// console.log(sum(1)(2)(3)(4)());
+
+const arr = [0, 1, 2, 3, 4, 5];
+const result = arr.filter((num) => num < 3);
+console.log(result);
+
+const p = document.querySelector('.number');
+const Timeout = () => {
+    let count = 0;
+    const t = setInterval(() => {
+        if (count <= 10) {
+            p.innerHTML = `<div>${count++}</div>`;
+        } else if (count >= 10) {
+            count = 0;
+        }
+
+        console.log(p);
+    }, 1000);
+};
+
+Timeout();
